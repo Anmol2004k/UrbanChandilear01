f97c63c34df3cd00b4202dd570fa9e841dc8a2e6
Anmol Kumar <at4141anmol@gmail.com>
2026-09-09 22:24:22 +0530

    start connecting DB

---
+++ includes/db_connect.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$host = "localhost";
+$dbname = "YOUR_DATABASE_NAME";
+$username = "YOUR_DATABASE_USERNAME";
+$password = "YOUR_DATABASE_PASSWORD";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed.");
+}
+
+$conn->set_charset("utf8mb4");
+
+?>
